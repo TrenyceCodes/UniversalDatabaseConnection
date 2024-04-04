a67b88220014ffc75d24eafe49dda926954738d8
Neice <52137719+TrenyceCodes@users.noreply.github.com>
2024-04-04 05:09:06 -0400

    User can call query function from connection function

--- database/data-access/connections/database-connections.ts
+++ database/data-access/connections/database-connections.ts
@@ -14,13 +14,9 @@ export async function createSqlConnection(mysqlConnection: mySqlDatabaseConfigur
         database: mysqlConnection.databaseName
     });
 
-    try {
-        return await sqlConnection.connect(() => {
-            console.log("Sql Database Connected");
-        });
-    } catch (error) {
-        throw new Error("Sql database connection error" + error);
-    }
+    await sqlConnection.connect();
+
+    return sqlConnection;
 }
 
 export async function createFirebaseConnection(firebaseDatabaseConfiguration: firebaseDatabaseConfiguration) {
--- database/index.ts
+++ database/index.ts
@@ -1,13 +1,15 @@
+import { log } from "console";
 import { SupportedDatabases, firebaseDatabaseConfiguration, mySqlDatabaseConfiguration } from "./data-access/configurations/database-configurations"
 import { createFirebaseConnection, createSqlConnection } from "./data-access/connections/database-connections";
 
-export async function UniversalDatabaseConnection(chosenDatabase: string, databaseConfigurations: mySqlDatabaseConfiguration | firebaseDatabaseConfiguration): Promise<any> {
+type DatabaseConnectionType = mySqlDatabaseConfiguration | firebaseDatabaseConfiguration | any;
+
+export async function UniversalDatabaseConnection(chosenDatabase: string, databaseConfigurations: DatabaseConnectionType): Promise<DatabaseConnectionType> {
     switch (chosenDatabase) {
         case SupportedDatabases.Sql:
-            return await createSqlConnection(databaseConfigurations as mySqlDatabaseConfiguration);
-            break;
+            return await createSqlConnection(databaseConfigurations);
         case SupportedDatabases.Firebase:
-            return await createFirebaseConnection(databaseConfigurations as firebaseDatabaseConfiguration);
+            return await createFirebaseConnection(databaseConfigurations);
         default:
             throw new Error(`Unsupported databases ${chosenDatabase}`);
     }
@@ -17,8 +19,24 @@ const databaseConfiguration: mySqlDatabaseConfiguration = {
     host: "localhost",
     user: "root",
     password: "password",
-    databaseName: "TaskifyDB"
+    databaseName: "MedicineDoseTracker"
+} 
+
+async function initializeConnection(supportedDatabase: SupportedDatabases, databaseConfiguration: DatabaseConnectionType) {
+    try {
+        const connection = await UniversalDatabaseConnection(supportedDatabase, databaseConfiguration);
+        connection.query('SELECT medicineName FROM Medicines', (error: any, results: any) => {
+            if (error) {
+                console.error("Error in query sql command", error);
+                return;
+            }
+            console.log("Query results: ", results);
+        })
+        console.log("Database connected successfully");
+        console.log(connection);
+    } catch (error) {
+        console.error("Error connecting to the database:", error);
+    }
 }
 
-const connection = UniversalDatabaseConnection(SupportedDatabases.Sql, databaseConfiguration);
-console.log(connection);
+initializeConnection(SupportedDatabases.Sql, databaseConfiguration);
